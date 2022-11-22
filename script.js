3e4d395f10c7cc9a76199c678cbdb8449b4d3c85
llpingll <94099281+llpingll@users.noreply.github.com>
2022-11-22 11:28:14 +0000

    Add dynamic grid size | add button toggles | add color picker

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const sliderProperties = {
     value: "16",
     min: "1",
-    max: "100"
+    max: "64"
 };
 
 
@@ -10,6 +10,9 @@ const sliderProperties = {
 const slider = document.getElementById("myRange");
 const gridDisplay = document.getElementById("grid");
 const colorPicker = document.querySelector(".color-picker");
+const grid = document.querySelector(".display");
+const buttons = document.querySelectorAll(".controls .toggle");
+const clear = document.querySelector("#clear");
 
 
 // Functions definitions
@@ -19,11 +22,41 @@ function setAttributes(el, attr) {
     }
 }
 
+function addGrid(numberOfBlocks) {
+    grid.style.gridTemplateColumns = `repeat(${numberOfBlocks}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${numberOfBlocks}, 1fr)`;
+    
+    numberOfBlocks2 = numberOfBlocks * numberOfBlocks;
+    for (let i = 0; i < numberOfBlocks2; i++) {
+        const div = document.createElement("div");
+        grid.appendChild(div);
+    }
+}
 
-// Set slider properties
-setAttributes(myRange, sliderProperties)
-// Event
+
+// Set initial slider properties
+setAttributes(slider, sliderProperties);
+// Set initial grid
+addGrid(slider.value);
+
+// Slider event
 slider.oninput = function() {
   slider.setAttribute("value", this.value)
   gridDisplay.textContent = `Grid size: ${this.value} x ${this.value}`;
+  addGrid(this.value);
 }
+
+// Add event listener for each button
+buttons.forEach(btn => btn.addEventListener("click", function() {
+    if (btn.value === "off") {
+        buttons.forEach(btn => {
+            btn.value = "off";
+            btn.classList.remove("on");
+        });
+        btn.value = "on";
+        btn.classList.add("on");
+    }
+}));
+
+// Add event listener to clear button
+clear.addEventListener("click", addGrid(slider.value));
